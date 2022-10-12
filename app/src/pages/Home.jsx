00ba5f557ed307b55b5a4d8b5544eda72c66b919
charlesncn <charowcjn@gmail.com>
2022-10-12 07:01:37 +0300

    created single post view

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-function Home() {
+const Home = () => {
+  const post = [
+    {
+      id:1,
+      title: 'How to build a website',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
+      img: 'https://img.freepik.com/free-vector/web-design-production-maintenance-websites-web-graphic-interface-design-responsive-website-software-engineering-development-colorful-icon_335657-2699.jpg?w=2000 ',
+    },
+    {
+      id:2,
+      title: 'How to build a website',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Quisquam, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
+      img: 'https://i2.wp.com/edulogy.pk/wp-content/uploads/2021/10/designing.jpg?fit=626%2C417&ssl=1',
+    }
+  ]
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className="posts">
+        {post.map(post=>(
+          <div className="post" key={post.id}>
+            <div className="img">
+              <img src={post.img} alt="" />
+            </div>
+            <div className="content">
+              <Link className='link' to={`/post:${post.id}`}>
+                <h3>{post.title}</h3>
+              </Link>
+              <p>{post.description}</p>
+              <Link className='readMore' to={`/post:${post.id}`}>Read More</Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
--- app/src/pages/Single.jsx
+++ app/src/pages/Single.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
 
-function Single() {
+const Single = () => {
+  const post = [
+    {
+      id: 1,
+      title: "How to build a website",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+      img: "https://img.freepik.com/free-vector/web-design-production-maintenance-websites-web-graphic-interface-design-responsive-website-software-engineering-development-colorful-icon_335657-2699.jpg?w=2000",
+      userIcon:
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+      username: "John Doe",
+    },
+  ];
   return (
-    <div>Single</div>
-  )
-}
+    <div className="post-cotainer">
+      {post.map(post => (
+        <div className="single-post" key={post.id}>
+          <div className="content">
+            <img src={post.img} alt="" />
+            <div className="user">
+              <img src={post.userIcon} alt="" />
+              <div className="info">
+                <span>{post.username}</span>
+                <p>posted 11 hrs ago.</p>
+              </div>
+            </div>
+          </div>
+          <div className="menu"></div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Single
+export default Single;
