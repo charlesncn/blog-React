4847e3b250287a6ec11743e5d191b9f907b20d53
charlesncn <charowcjn@gmail.com>
2022-10-14 20:22:55 -0700

    cleaning code

--- app/src/componests/Footer.jsx
+++ app/src/componests/Footer.jsx
@@ -1,36 +1,62 @@
-import React from 'react'
-import Logo from '../img/logo.png';
-import { Link } from 'react-router-dom'
+import React from "react";
+import Logo from "../img/logo.png";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div className='footer'>
+    <div className="footer">
       <div className="logo">
-        <Link to='/'> <img src={Logo} alt=""/> </Link>
+        <Link to="/">
+          {" "}
+          <img src={Logo} alt="" />{" "}
+        </Link>
       </div>
       <div className="message">
-        <form >
+        <form>
           <span>Leave a Message</span>
-          <input type="text" placeholder="Enter your email"/>
-          <textarea name="" id="" cols="30" rows="5" placeholder="Enter your message"></textarea>
+          <input type="text" placeholder="Enter your email" />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="5"
+            placeholder="Enter your message"
+          ></textarea>
           <div className="btns">
-          <button>Send</button>
-          <button>Discard</button>
+            <button>Send</button>
+            <button>Discard</button>
           </div>
         </form>
       </div>
       <div className="socialmedia">
         <ul>
-          <li><Link to=''/>Github</li>
-          <li><Link to=''/>Linkedin</li>
-          <li><Link to=''/>Website</li>
-          <li><Link to=''/>Stackoverflow</li>
-          <li><Link to=''/>Instagram</li>
+          <li>
+            <Link to="" />
+            Github
+          </li>
+          <li>
+            <Link to="" />
+            Linkedin
+          </li>
+          <li>
+            <Link to="" />
+            Website
+          </li>
+          <li>
+            <Link to="" />
+            Stackoverflow
+          </li>
+          <li>
+            <Link to="" />
+            Instagram
+          </li>
         </ul>
       </div>
-        <p>© 2021 All rights reserved. Designed by <a href="">Njuguna</a></p>
+      <p>
+        © 2021 All rights reserved. Designed by <a href="">Njuguna</a>
+      </p>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
