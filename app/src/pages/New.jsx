d1dbdad16decefc9b3a7d2ded5c76cd0ed69e3ae
charlesncn <charowcjn@gmail.com>
2022-10-16 08:24:38 +0300

    New post styles

--- app/src/pages/New.jsx
+++ app/src/pages/New.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function New() {
   return (
-    <div>New</div>
+    <div className='create'>
+      <div className="content">
+        content
+      </div>
+        <div className="menu">
+          <div className="menu-item">2</div>
+          <div className="menu-item">3</div>
+        </div>
+    </div>
   )
 }
 
